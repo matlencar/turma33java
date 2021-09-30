536309bdefc2ce4bd687396e1bf346bbb570edad
matlencar <malencar292@gmail.com>
2021-09-30 10:25:56 -0300

    Novo projeto

---
+++ Spring Boot/Loja de jogos/ecommerceGames/ecommerceGames/src/main/java/com/minhaLojaDeGames/ecommerceGames/EcommerceGamesApplication.java
@@ -0,0 +1,13 @@
+package com.minhaLojaDeGames.ecommerceGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGamesApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/Loja de jogos/ecommerceGames/ecommerceGames/src/test/java/com/minhaLojaDeGames/ecommerceGames/EcommerceGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaLojaDeGames.ecommerceGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
