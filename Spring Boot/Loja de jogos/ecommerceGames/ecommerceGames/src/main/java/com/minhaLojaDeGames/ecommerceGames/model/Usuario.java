faa927a6d07a8a124a20a3cb0ff87a23eecf8f86
matlencar <malencar292@gmail.com>
2021-10-08 07:11:48 -0300

    blog e loja de games

---
+++ Spring Boot/Loja de jogos/ecommerceGames/ecommerceGames/src/main/java/com/minhaLojaDeGames/ecommerceGames/model/Usuario.java
@@ -0,0 +1,24 @@
+package com.minhaLojaDeGames.ecommerceGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 25)
+	private String nome;
+	
+}
