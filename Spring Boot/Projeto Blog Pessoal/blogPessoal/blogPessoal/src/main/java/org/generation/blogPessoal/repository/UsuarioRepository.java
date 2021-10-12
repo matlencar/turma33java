f5b5dcb823c26968c3efd0470b886080b7990fff
matlencar <malencar292@gmail.com>
2021-10-11 21:56:14 -0300

    JUnit Teste

--- Spring Boot/Projeto Blog Pessoal/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ Spring Boot/Projeto Blog Pessoal/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package org.generation.blogPessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.Usuario;
@@ -7,4 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	public Optional<Usuario> findByUsuario(String Usuario);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	
+	public Usuario findByNome(String nome);
 }
